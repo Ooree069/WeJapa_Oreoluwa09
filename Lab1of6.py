66b311460a53654bbc17eb8a14f3a17eceb10e25
Ooree069 <66417658+Ooree069@users.noreply.github.com>
2020-07-28 09:05:46 +0100

    Add files via upload from phone

---
+++ Lab1of6.py
@@ -0,0 +1,8 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print ("{} accessed the site {} at {}.".format (username, url, timestamp)) 
