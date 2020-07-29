19faae83de902cf400d4f49b30c054d24ff9ae86
Ooree069 <66417658+Ooree069@users.noreply.github.com>
2020-07-29 14:05:50 +0100

    Delete Lab1of1.py

--- Lab1of1.py
+++
@@ -1,9 +0,0 @@
-#Quiz: Average Electricity Bill
-#It's time to try a calculation in Python!
-a = 23
-b = 32
-c = 64
-# Write an expression that calculates the average of 23, 32 and 64
-# Place the expression in this print statement
-average = (a + b + c ) / 3
-print(average)
