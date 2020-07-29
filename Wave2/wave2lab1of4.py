2933e306c9c5b767f58434a8437efff3c9779969
Ooree069 <66417658+Ooree069@users.noreply.github.com>
2020-07-29 14:05:25 +0100

    Add files via upload

---
+++ Wave2/wave2lab1of4.py
@@ -0,0 +1,14 @@
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions of people.
+Populated_Cities = {"Shanghai": 17.8e6, "Istanbul": 13.4e6, "Karachi": 13.0e6, "Mumbai": 12.5e6} 
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+print(Populated_Cities) 
