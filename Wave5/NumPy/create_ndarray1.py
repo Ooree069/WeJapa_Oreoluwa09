9bfb0b8ebebafe3a45f8a77626f11ec3d4087765
Ooree069 <66417658+Ooree069@users.noreply.github.com>
2020-08-21 00:00:14 +0100

    Add files via upload

---
+++ Wave5/NumPy/create_ndarray1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Using the Built-in functions you learned about in the
+# previous lesson, create a 4 x 4 ndarray that only
+# contains consecutive even numbers from 2 to 32 (inclusive)
+
+X = np.arange(2,33,2).reshape(4,4)
+print(X)
